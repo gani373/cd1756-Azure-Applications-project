18fa5fa27dbf845cb53435e3b295e4aea6af90b7
gani373 <gg3466000@gmail.com>
2025-09-20 19:04:38 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,34 +1,19 @@
+"""
+The flask application package.
+"""
 import logging
-import os
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__)
-
-# Read configuration from environment variables
-app.config.update(
-    CLIENT_ID=os.environ.get("CLIENT_ID"),
-    CLIENT_SECRET=os.environ.get("CLIENT_SECRET"),
-    AUTHORITY=os.environ.get("AUTHORITY"),
-    REDIRECT_PATH=os.environ.get("REDIRECT_PATH", "/redirect"),
-    SCOPE=os.environ.get("SCOPE", "User.ReadBasic.All").split(' '),
-    SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    BLOB_ACCOUNT=os.environ.get("BLOB_ACCOUNT"),
-    BLOB_CONTAINER=os.environ.get("BLOB_CONTAINER"),
-    SESSION_TYPE=os.environ.get("SESSION_TYPE", "filesystem"),
-    SECRET_KEY=os.environ.get("SECRET_KEY")
-)
-
-# Add logging levels and handlers
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-app.logger.addHandler(handler)
-app.logger.setLevel(logging.INFO)
-
+app.config.from_object(Config)
+wsgi_app = app.wsgi_app
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
