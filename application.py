75820eb64e1628d6b86ab8fd47a8868e9e0d5289
gani373 <gg3466000@gmail.com>
2025-09-20 19:22:31 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,11 @@
 """
-This script runs the FlaskWebProject application using a development server.
+This script runs the FlaskWebProject application using a WSGI server on Azure.
 """
 
-from os import environ
 from FlaskWebProject import app
 
+# This is a standard entry point for WSGI servers
+# app is the Flask application instance
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+    # This block is for local development only
+    app.run()
